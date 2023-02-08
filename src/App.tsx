4e02a8c8a9b8c2b714e67324cb07864ae0e8c2d8
arbelais <arbelaism@outlook.com>
2023-02-08 10:21:47 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { type ReactElement } from 'react'
+
+function App (): ReactElement {
+  return (
+        <div className="w-full h-screen grid place-content-center">
+            <h1 className='text-6xl text-slate-200 font-bold'>StarWars Battle App</h1>
+        </div>
+  )
+}
+
+export default App
